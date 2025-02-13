ebd2bef080f22e53547f0e4ac503d055223a5e9d
JoaoVictorCRP <joaovictorxl120@gmail.com>
2025-02-12 22:02:53 -0300

    o basico do react e web router

---
+++ 0-introducao-ao-next/first-app/app/aboutUs/page.jsx
@@ -0,0 +1,17 @@
+export default function Page () {
+    const description = "Estamos aprendendo Next.js!"
+    const element = <h3>3 vezes 3 é {3*3}</h3>
+
+    return ( <div style={{'textAlign':'center'}}>
+        <h1>Sobre Nós</h1>
+        <p>
+            {description}
+        </p>
+
+        <footer style={{'position':'absolute','bottom':'0px','display':'inline','textAlign':'center'}}>
+            {element}
+        </footer>
+      </div>
+    //   O web router permite que a própria pasta seja uma rota na aplicação.   
+    )
+}
---
+++ 0-introducao-ao-next/first-app/app/page.jsx
@@ -0,0 +1,8 @@
+export default function Page () {
+    return ( <div style={{'textAlign':'center'}}>
+        <h1>Programação Script</h1>
+        <h3>Com Professor Leonardo</h3>
+        <p>Sobre Nós</p>
+      </div>
+    )
+}
